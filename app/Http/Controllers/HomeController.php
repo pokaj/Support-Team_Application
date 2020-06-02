00366a014d10cac39c19c822997cb91e7a0763d8
philip_owusu_afriyie <philip.afriyie@ashesi.edu.gh>
2020-06-02 15:44:13 +0000

    Complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $pending = count(DB::table('activities')
+        ->where('activity_status', '=', 'pending')
+        ->get());
+
+        $complete = count(DB::table('activities')
+        ->where('activity_status', '=', 'complete')
+        ->get());
+
+        return view('pages/dashboard',compact('complete','pending'));
     }
 }
